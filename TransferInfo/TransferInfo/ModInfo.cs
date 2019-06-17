7a774b7339ea9970493f719776e4e00e66ff99b1
Vladimir Poteryaev <vpoteryaev@gmail.com>
2019-06-18 08:02:38 +1000

    Added VS solution

---
+++ TransferInfo/TransferInfo/ModInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ICities;
+
+namespace TransferInfo
+{
+    public class ModInfo: IUserMod
+    {
+        public string Name => "Transfer Info";
+        public string Description => "bla-bla-bla";
+    }
+}
