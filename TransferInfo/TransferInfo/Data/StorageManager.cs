223cc079f001b1e9115f71e84df9f0097ae5dd16
Vladimir Poteryaev <vpoteryaev@gmail.com>
2019-06-26 03:57:59 +1000

    Basic functionality with loading/saving the game. Fix AB#16

---
+++ TransferInfo/TransferInfo/Data/StorageManager.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using System.Runtime.Serialization;
+using ICities;
+using UnityEngine;
+
+namespace TransferInfo.Data
+{
+    internal class StorageManager: SerializableDataExtensionBase
+    {
+        internal static StorageManager Instance { get; private set; }
+
+        internal TransfersStatistics Data { get; private set; }
+
+        internal StorageManager()
+        {
+            Data = new TransfersStatistics(Options.StorageVersion);
+            Instance = this;
+        }
+
+        public override void OnCreated(ISerializableData serializableData)
+        {
+            base.OnCreated(serializableData);
+        }
+
+        public override void OnLoadData()
+        {
+            var data = serializableDataManager.LoadData(Options.GameStorageID);
+            if(data == null)
+            {
+#if DEBUG
+                Debug.Log("TransferInfo: StorageManager.OnLoadData - data in saved game not found.");
+#endif
+                return;
+            }
+            MemoryStream stream = new MemoryStream(data);
+            try
+            {
+                BinaryFormatter formatter = new BinaryFormatter();
+                var tempData = formatter.Deserialize(stream);
+                if (tempData is TransfersStatistics tempStorageData)
+                    if (tempStorageData.version == Options.StorageVersion)
+                        Data = tempStorageData;
+#if DEBUG
+                    else
+                        Debug.Log("TransferInfo: StorageManager.OnLoadData - wrong version.");
+                else
+                    Debug.Log("TransferInfo: StorageManager.OnLoadData - wrong data format.");
+#endif
+            }
+            catch (SerializationException e)
+            {
+                Debug.LogError("TransferInfo: StorageManager.OnLoadData - Failed to deserialize. Reason: " + e.Message);
+                //throw;
+            }
+            //finally
+            //{
+            //}
+
+            base.OnLoadData();
+        }
+
+        public override void OnSaveData()
+        {
+            MemoryStream stream = new MemoryStream();
+            BinaryFormatter formatter = new BinaryFormatter();
+            try
+            {
+                formatter.Serialize(stream, Data);
+            }
+            catch (SerializationException e)
+            {
+                Debug.LogError("TransferInfo: StorageManager.OnLoadData - Failed to serialize. Reason: " + e.Message);
+                //throw;
+            }
+            //finally
+            //{
+            //}
+
+            serializableDataManager.SaveData(Options.GameStorageID, stream.ToArray());
+
+            base.OnSaveData();
+        }
+
+        public override void OnReleased()
+        {
+            base.OnReleased();
+        }
+    }
+}
